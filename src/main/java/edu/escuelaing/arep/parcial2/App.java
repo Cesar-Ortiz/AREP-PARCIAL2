f8e32896fc1c2fcf1433c1ffc9029652efb693af
César Ortiz <cesar.ortiz@mail.escuelaing.edu.co>
2021-10-13 16:09:03 -0500

    agregando llaves y seguridad https

--- src/main/java/edu/escuelaing/arep/parcial2/App.java
+++ src/main/java/edu/escuelaing/arep/parcial2/App.java
@@ -11,6 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        secure("keystores/ecikeystore.p12", "123456", null, null);
         port(getPort());
         get("/hello", (req, res) -> "Hello World");
         Operations ope = new Operations();
@@ -40,6 +41,6 @@ public class App
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
-        return 4567; //returns default port if heroku-port isn't set (i.e. on localhost)
+        return 6000; //returns default port if heroku-port isn't set (i.e. on localhost)
     }
 }
